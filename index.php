8c392c5a9c29ff0cf44da5a10e5192c8ba5db250
Manish srivas <76111668+manishdeveloper333@users.noreply.github.com>
2022-11-12 14:11:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ if (isset($_POST['submit'])) {
         <div class="container px-5">
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6 order-lg-2">
-                    <div class="p-5"><img class="img-fluid " src="01.svg" alt="manishdeveloper" /></div>
+                    <div class="p-5"><img class="img-fluid " src="/01.svg" alt="manishdeveloper333" /></div>
                 </div>
                 <div class="col-lg-6 order-lg-1">
                     <div class="p-5">
@@ -96,7 +96,7 @@ if (isset($_POST['submit'])) {
         <div class="container px-5">
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6">
-                    <div class="p-5"><img class="img-fluid " src="02.svg" alt="manishdeveloper" /></div>
+                    <div class="p-5"><img class="img-fluid " src="02.svg" alt="manishdeveloper333" /></div>
                 </div>
                 <div class="col-lg-6">
                     <div class="p-5">
@@ -115,7 +115,7 @@ if (isset($_POST['submit'])) {
         <div class="container px-5">
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6 order-lg-2">
-                    <div class="p-5"><img class="img-fluid " src="03.svg" alt="manishdeveloper" /></div>
+                    <div class="p-5"><img class="img-fluid " src="03.svg" alt="manishdeveloper333" /></div>
                 </div>
                 <div class="col-lg-6 order-lg-1">
                     <div class="p-5">
